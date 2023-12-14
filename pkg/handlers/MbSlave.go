2484b9e99c81f2c97a34d5db557b18137633d673
dshargool <dshargool2009@gmail.com>
2023-12-13 17:21:52 -0700

    Rework to use data of any type let client handle different types if unspecified
    Lint

--- pkg/handlers/MbSlave.go
+++ pkg/handlers/MbSlave.go
@@ -52,31 +52,27 @@ func (h *Handler) HandleDiscreteInputs(req *modbus.DiscreteInputsRequest) (res [
 }
 
 func (h *Handler) HandleHoldingRegisters(req *modbus.HoldingRegistersRequest) (res []uint16, err error) {
-	// Write to DB entry with matching address.  Only update don't insert as the DbHandler should do the inserting of null values
+	// Write to DB entry with matching address.
+	// Only update don't insert as the DbHandler should do the inserting of null values
 	slog.Info("HandleHoldingRegisters - new request", "req", req)
-	var dataType string = "none"
-	for i := 0; i < int(req.Quantity); i++ {
-        // Move our request address along to service the entire quantity
+	var dataType string
+	i := 0
+	for i < int(req.Quantity) {
+		// Move our request address along to service the entire quantity
 		regAddr := req.Addr + uint16(i)
 
-        // If our dataType is uninitialized we try to do it from the database. 
-        // If it fails here we don't know what type of data to expect to read and it will fail
-		if dataType == "none" {
-			dataType, err = h.db.GetDataTypeByAddress(int(regAddr))
-			if err != nil {
-				slog.Error("Unable to read row data type", "address", regAddr,
-					"allow_null", h.AllowNullRegisters, "req", req, "err", err)
-				return res, modbus.ErrProtocolError
-			}
-		}
-
-        // If the requested quantity is an odd number of bits we have to make 
-        // it an integer type as floats are 2 or 4 bytes wide
-		if req.Quantity%2 != 0 {
+		// If our dataType is uninitialized we try to do it from the database.
+		// If it fails here we don't know what type of data to expect to read and it will fail
+		dataType, err = h.db.GetDataTypeByAddress(int(regAddr))
+		if err != nil && !h.AllowNullRegisters {
+			slog.Error("Unable to read row data type", "address", regAddr,
+				"allow_null", h.AllowNullRegisters, "req", req, "err", err)
+			return res, modbus.ErrProtocolError
+		} else if dataType == "none" && h.AllowNullRegisters {
 			dataType = "uint16"
 		}
 
-        // Based on the data type get the number of registers we'll return
+		// Based on the data type get the number of registers we'll return
 		num_regs, err := numRegsDataType(dataType)
 		if err != nil {
 			slog.Error("Unable to calculate number of required registers for datatype.",
@@ -87,12 +83,12 @@ func (h *Handler) HandleHoldingRegisters(req *modbus.HoldingRegistersRequest) (r
 		if req.IsWrite {
 			slog.Info("Writing holding registers", "address", regAddr)
 
-            // Put our arguments that we're interested in into a new data slice
+			// Put our arguments that we're interested in into a new data slice
 			var data []uint16
 			for j := 0; j < int(num_regs); j++ {
 				data = append(data, req.Args[i+j])
 			}
-            // Convert the bytes of our slice to our data type
+			// Convert the bytes of our slice to our data type
 			conv_val, err := parseByteToDataType(dataType, req.Args)
 			if err != nil {
 				slog.Error("Unable to convert data type",
@@ -103,7 +99,7 @@ func (h *Handler) HandleHoldingRegisters(req *modbus.HoldingRegistersRequest) (r
 			slog.Info("Updating database with holding registers",
 				"address", regAddr, "data", data, "value", conv_val)
 
-            // Write the value we received into the DB
+			// Write the value we received into the DB
 			err = h.db.SetAddressValue(int(regAddr), conv_val)
 			if err != nil {
 				slog.Error("Unable to update database with holding registers",
@@ -111,40 +107,39 @@ func (h *Handler) HandleHoldingRegisters(req *modbus.HoldingRegistersRequest) (r
 				return res, modbus.ErrProtocolError
 			}
 
-			i = i + int(num_regs) - 1
-
 		} else {
 			slog.Info("Reading holding registers", "address", regAddr)
 
 			// Get the current value from the database
 			current, err := h.db.GetRowByAddress(int(regAddr))
 			if err != nil {
-				slog.Error("Unable to read from database",
-					"address", regAddr, "error", err.Error())
 				// When we don't have a database value but allow null registers we return a 0
-                // if we don't allow null values it's considered an illegal data address
+				// if we don't allow null values it's considered an illegal data address
 				if h.AllowNullRegisters {
 					slog.Warn("Setting Null Register to 0")
 					current.Value = 0
 				} else {
+					slog.Error("Unable to read from database",
+						"address", regAddr, "error", err.Error())
 					return res, modbus.ErrIllegalDataAddress
 				}
 			}
 
 			// Take our value and parse it into the datatype we expect to use
-			conv_val, err := parseDataTypeToByte(dataType, float64(current.Value))
+			conv_val, err := parseDataTypeToByte(dataType, current.Value)
 			if err != nil {
 				slog.Error("Couldn't parse DataType to Byte",
 					"DataType", dataType)
 			}
-			// Increment the addresses by the amount we're appending minus the regular increase
-			i = i + int(num_regs) - 1
+			slog.Info("Adding value to result", "value", conv_val)
 			res = append(res, conv_val...)
 		}
+		// Increment the addresses by the amount we're appending
+		i = i + int(num_regs)
 	}
 	slog.Info("HandleHoldingRegisters - Returning data",
 		"length", len(res), "request_len", req.Quantity, "error", err)
-	return
+	return res, nil
 }
 
 func (h *Handler) HandleInputRegisters(req *modbus.InputRegistersRequest) (res []uint16, err error) {
