ca8149efd20ff35e73c3aaa1af4d9a4ef8c50674
dshargool <dshargool2009@gmail.com>
2023-12-25 19:24:33 -0700

    Add status to main page

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,17 +1,17 @@
 <script>
-  import '../app.css'
+	import '../app.css';
 </script>
 
 <div class="navbar gap-4">
-<div class="flex-none">
-	<a class="btn" href="/">Home</a>
-</div>
-<div class="flex-1">
-	<a class="btn" href="/registers">Data</a>
-</div>
-<div class="flex-none">
-	<a class="btn" href="/settings">Settings</a>
-</div>
+	<div class="flex-none">
+		<a class="btn" href="/">Home</a>
+	</div>
+	<div class="flex-1">
+		<a class="btn" href="/registers">Data</a>
+	</div>
+	<div class="flex-none">
+		<a class="btn" href="/settings">Settings</a>
+	</div>
 </div>
 
 <slot />
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,1 +1,48 @@
-<h1>Modbus API</h1>
+<script>
+	/** @type {import('./$types').PageData}*/
+	import { onMount } from 'svelte';
+
+	function getApiStatus() {
+		return true;
+	}
+
+	function updateStatus() {
+		console.log('updating status');
+		const api_status = getApiStatus();
+		const elem = document.getElementById('api-status');
+		if (elem == null) {
+			return;
+		}
+
+		if (api_status == true) {
+			if (elem.classList.contains('badge-error')) {
+				elem.classList.remove('badge-error');
+			}
+			elem.classList.add('badge-success');
+		} else {
+			if (elem.classList.contains('badge-success')) {
+				elem.classList.remove('badge-success');
+			}
+			elem.classList.add('badge-error');
+		}
+		return;
+	}
+
+	onMount(async () => {
+		setInterval(() => {
+			updateStatus();
+		}, 10000);
+	});
+</script>
+
+<div class="flex flex-col min-h-screen justify-center gap-9 px-9 items-center">
+	<h1>Modbus API</h1>
+	<h3>Status</h3>
+	<div class="indicator">
+		<span
+			use:updateStatus
+			id="api-status"
+			class="indicator-item indicator-middle indicator-center badge badge-lg"
+		></span>
+	</div>
+</div>
