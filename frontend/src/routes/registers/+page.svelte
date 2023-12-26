b9b3975980866fedea6eb48bd5f02117a469bdb1
dshargool <dshargool2009@gmail.com>
2023-12-25 19:24:24 -0700

    Clean registers page

--- frontend/src/routes/registers/+page.svelte
+++ frontend/src/routes/registers/+page.svelte
@@ -2,63 +2,59 @@
 	/** @type {import('./$types').PageData}*/
 	export let data;
 
-    function sortRegisters(multiplier) {
-        data.data.sort((a,b) => { 
-            if (a.address > b.address) {
-                return 1 * multiplier;
-            }
-            if (a.address < b.address) {
-                return -1 * multiplier;
-            } 
-            return 0;
-        })
-        data.data = data.data; // Refresh data
-    }
 	import { invalidateAll } from '$app/navigation';
 	import { onMount } from 'svelte';
 
-	function rerunLoadFunction() {
-		// any of these will cause the `load` function to rerun
-		invalidateAll();
+	function sortRegisters(multiplier = 1) {
+		data.data.sort((a, b) => {
+			if (a.address > b.address) {
+				return 1 * multiplier;
+			}
+			if (a.address < b.address) {
+				return -1 * multiplier;
+			}
+			return 0;
+		});
+		data.data = data.data; // Refresh data
 	}
 
-    onMount(async () => {
-        setInterval(() => {
-            rerunLoadFunction();
-            sortRegisters(1);
-        }, 10000);
-    });
+	onMount(async () => {
+		setInterval(() => {
+			invalidateAll();
+			sortRegisters(1);
+		}, 10000);
+	});
 
-
-
+	// Start doing things
+	sortRegisters(1);
 </script>
 
 <main>
-	<h1 class="font-bold text-2xl">Registers</h1>
-    <div class="overflow-x-auto">
-	<table class="table table-zebra table-pin-rows">
-		<thead>
-			<tr>
-				<th>Tag</th>
-				<th>Description</th>
-				<th>Address</th>
-				<th>Value</th>
-				<th>Last Update</th>
-			</tr>
-		</thead>
-        <tbody>
-		{#if data.data}
-			{#each data.data as register}
+	<h3>Registers</h3>
+	<div class="overflow-x-auto">
+		<table class="table table-zebra table-pin-rows">
+			<thead>
 				<tr>
-					<td>{register.tag}</td>
-					<td>{register.description}</td>
-					<td>{register.address}</td>
-					<td>{register.value}</td>
-					<td>{register.last_update}</td>
+					<th>Tag</th>
+					<th>Description</th>
+					<th>Address</th>
+					<th>Value</th>
+					<th>Last Update</th>
 				</tr>
-			{/each}
-		{/if}
-        </tbody>
-	</table>
-    </div>
+			</thead>
+			<tbody>
+				{#if data.data}
+					{#each data.data as register}
+						<tr>
+							<td>{register.tag}</td>
+							<td>{register.description}</td>
+							<td>{register.address}</td>
+							<td>{register.value}</td>
+							<td>{register.last_update}</td>
+						</tr>
+					{/each}
+				{/if}
+			</tbody>
+		</table>
+	</div>
 </main>
