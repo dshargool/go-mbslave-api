e9714a2665390ba0765b22aaf4d8ed50406a0e4f
dshargool <dshargool2009@gmail.com>
2025-08-15 09:47:05 -0600

    Update sort to handle underscore addresses

--- frontend/src/routes/registers/+page.svelte
+++ frontend/src/routes/registers/+page.svelte
@@ -5,18 +5,33 @@
 	import { invalidateAll } from '$app/navigation';
 	import { onMount } from 'svelte';
 
-	function sortRegisters(multiplier = 1) {
-		data.data.sort((a, b) => {
-			if (a.address > b.address) {
-				return 1 * multiplier;
-			}
-			if (a.address < b.address) {
-				return -1 * multiplier;
-			}
-			return 0;
-		});
-		data.data = data.data; // Refresh data
-	}
+    function sortRegisters(multiplier = 1) {
+        data.data.sort((a, b) => {
+            const addressA = a.address.toString();
+            const addressB = b.address.toString();
+            
+            // Split addresses on underscore
+            const partsA = addressA.split('_');
+            const partsB = addressB.split('_');
+            
+            // Get the first part (before underscore) as numbers
+            const primaryA = parseInt(partsA[0]);
+            const primaryB = parseInt(partsB[0]);
+            
+            // Primary sort: compare the first part numerically
+            if (primaryA !== primaryB) {
+                return (primaryA - primaryB) * multiplier;
+            }
+            
+            // Secondary sort: if first parts are equal, compare second parts
+            // If no second part exists, treat as 0 for sorting purposes
+            const secondaryA = partsA.length > 1 ? parseInt(partsA[1]) || 0 : 0;
+            const secondaryB = partsB.length > 1 ? parseInt(partsB[1]) || 0 : 0;
+            
+            return (secondaryA - secondaryB) * multiplier;
+        });
+        data.data = data.data; // Refresh data
+    }
 
 	onMount(async () => {
 	    sortRegisters(1);
